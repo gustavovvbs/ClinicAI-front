0ccde0e992bfcccaa1861d46e8b81dc85c5144a6
gustavovvbs <gustavovvbs@al.insper.edu.br>
2024-12-06 17:57:47 -0300

    chrore: fixing url

--- clinicai-front/src/services/api.ts
+++ clinicai-front/src/services/api.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL = "https://sprint-hsl-first-c8b5d7dddb20.herokuapp.com"; 
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 // const API_BASE_URL = "http://localhost:8000"; 
 
